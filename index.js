5b29d2edb03bf50c10f6104eee511cb215df3f90
Alex <alexleka98@outlook.com>
2021-02-21 18:45:39 +0200

    Initialize everything

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const ejsMate = require('ejs-mate');
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+
+const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/red-rain";
+mongoose.connect(dbUrl, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+});
+
+//Check if there is an error when connecting with the database
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+
+app.engine('ejs', ejsMate);
+app.set("view engine", "ejs");
+app.use(methodOverride('_method'));
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/assets'));
+
+
+app.get('/home', (req, res) => {
+    res.render('home');
+})
+
+
+
+app.listen(3000, () => {
+    console.log(`Server on port 3000`);
+});
