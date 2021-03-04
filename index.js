0dcbd57d649c1a5cbca9bc369d4ade5ee69e9be4
Alex <alexleka98@outlook.com>
2021-03-04 23:39:28 +0200

    .env file

--- index.js
+++ index.js
@@ -28,6 +28,13 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/assets'));
 
 
+
+
+
+
+
+
+
 app.get('/home', (req, res) => {
     res.render('home');
 })
@@ -66,37 +73,6 @@ app.get("/talent", (req, res) => {
 app.get("/about-talent", (req, res) => {
     res.render("./talent-agency/about-talent")
 })
-
-
-
-
-
-
-const YOUR_DOMAIN = 'http://localhost:3000';
-
-app.post('/create-checkout-session', async (req, res) => {
-    const session = await stripe.checkout.sessions.create({
-        payment_method_types: ['card'],
-        line_items: [
-            {
-                price_data: {
-                    currency: 'usd',
-                    product_data: {
-                        name: 'Stubborn Attachments',
-                        images: ['https://i.imgur.com/EHyR2nP.png'],
-                    },
-                    unit_amount: 2000,
-                },
-                quantity: 1,
-            },
-        ],
-        mode: 'payment',
-        success_url: `${YOUR_DOMAIN}/success.html`,
-        cancel_url: `${YOUR_DOMAIN}/cancel.html`,
-    });
-
-    res.json({ id: session.id });
-});
 
 
 app.listen(3000, () => {
