51417cf4b1255758639f61d4b2391f25531d79e9
Alex <alexleka98@outlook.com>
2021-03-04 04:45:19 +0200

    Minor changes

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const ejsMate = require('ejs-mate');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
+const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
 
 const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/red-rain";
 mongoose.connect(dbUrl, {
@@ -66,6 +67,36 @@ app.get("/about-talent", (req, res) => {
     res.render("./talent-agency/about-talent")
 })
 
+
+
+
+
+
+const YOUR_DOMAIN = 'http://localhost:3000';
+
+app.post('/create-checkout-session', async (req, res) => {
+    const session = await stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        line_items: [
+            {
+                price_data: {
+                    currency: 'usd',
+                    product_data: {
+                        name: 'Stubborn Attachments',
+                        images: ['https://i.imgur.com/EHyR2nP.png'],
+                    },
+                    unit_amount: 2000,
+                },
+                quantity: 1,
+            },
+        ],
+        mode: 'payment',
+        success_url: `${YOUR_DOMAIN}/success.html`,
+        cancel_url: `${YOUR_DOMAIN}/cancel.html`,
+    });
+
+    res.json({ id: session.id });
+});
 
 
 app.listen(3000, () => {
